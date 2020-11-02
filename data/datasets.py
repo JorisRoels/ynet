da6fa3e26342a13aa36f7bd7a46bbb7b5d4fd377
Joris Roels <joris.roels@ugent.vib.be>
2020-11-02 16:55:43 +0100

    Added autoencoder script

--- data/datasets.py
+++ data/datasets.py
@@ -143,11 +143,11 @@ class VolumeDataset(data.Dataset):
 
         # select a crop of the data if necessary
         print_frm('Original dataset size: %d x %d x %d (total: %d)' % (
-        self.data.shape[0], self.data.shape[1], self.data.shape[2], self.data.size))
+            self.data.shape[0], self.data.shape[1], self.data.shape[2], self.data.size))
         if available >= 0:
             self.data = _select_subset(self.data, available=available)
         print_frm('Used for training: %d x %d x %d (total: %d)' % (
-        self.data.shape[0], self.data.shape[1], self.data.shape[2], self.data.size))
+            self.data.shape[0], self.data.shape[1], self.data.shape[2], self.data.size))
 
     def __getitem__(self, i):
         pass
@@ -299,6 +299,152 @@ class UnlabeledVolumeDataset(VolumeDataset):
         input = _orient(input, orientation=self.orientation)
 
         if self.input_shape[0] > 1:
+            # add channel axis if the data is 3D
+            return input[np.newaxis, ...]
+        else:
+            return input
+
+
+class MultiVolumeDataset(data.Dataset):
+    """
+    Dataset for multiple volumes
+
+    :param data_path: list of paths to the datasets
+    :param input_shape: 3-tuple that specifies the input shape for sampling
+    :param optional split_orientation: orientation to train/test split the data along
+    :param optional split_location: location where the train/test data should be split
+    :param optional scaling: tuple used for rescaling the data, or None
+    :param optional len_epoch: number of iterations for one epoch
+    :param optional types: list of types of the volume files (tif2d, tif3d, tifseq, hdf5, png or pngseq)
+    :param optional in_channels: amount of subsequent slices to be sampled (only for 2D sampling)
+    :param optional sampling_mode: allow for uniform balance in sampling or not ("uniform" or "random")
+    :param optional orientations: list of orientations for sampling
+    :param optional batch_size: size of the sampling batch
+    :param optional dtype: type of the data (typically uint8)
+    :param optional norm_type: type of the normalization (unit, z or minmax)
+    :param optional train: train or test data
+    :param optional available: amount of available data and labels
+    """
+
+    def __init__(self, data_path, input_shape, split_orientation='z', split_location=0.50, scaling=None, len_epoch=1000,
+                 types=['tif3d'], sampling_mode='uniform', in_channels=1, orientations=(0,), batch_size=1,
+                 dtype='uint8', norm_type='unit', train=True, available=-1):
+        self.data_path = data_path
+        self.input_shape = input_shape
+        self.scaling = scaling
+        self.len_epoch = len_epoch
+        self.sampling_mode = sampling_mode
+        self.in_channels = in_channels
+        self.orientations = orientations
+        self.orientation = 0
+        self.k = 0
+        self.batch_size = batch_size
+        self.norm_type = norm_type
+
+        # load the data
+        self.data = []
+        self.data_sizes = []
+        for k, path in enumerate(data_path):
+            print_frm('Loading dataset %d/%d: %s' % (k, len(data_path), path))
+
+            d = 0 if split_orientation[k] == 'z' else 1 if split_orientation[k] == 'y' else 2
+            if split_orientation[k] == 'z':
+                split = int(len(os.listdir(path)) * split_location[k])
+                start = 0 if train else split
+                stop = split if train else -1
+                data = read_volume(path, type=types[k], dtype=dtype, start=start, stop=stop)
+            else:
+                data = read_volume(path, type=types[k], dtype=dtype)
+                split = int(data.shape[d] * split_location[k])
+                if split_orientation[k] == 'y':
+                    data = data[:, :split, :] if train else data[:, split:, :]
+                else:
+                    data = data[:, :, :split] if train else data[:, :, split:]
+
+            # rescale the dataset if necessary
+            if scaling is not None:
+                target_size = np.asarray(np.multiply(data.shape, scaling), dtype=int)
+                data = F.interpolate(torch.Tensor(data[np.newaxis, np.newaxis, ...]), size=tuple(target_size),
+                                  mode='area')[0, 0, ...].numpy()
+
+            # select a crop of the data if necessary
+            print_frm('Original dataset size: %d x %d x %d (total: %d)' % (
+                data.shape[0], data.shape[1], data.shape[2], data.size))
+            if available >= 0:
+                data = _select_subset(data, available=available)
+            print_frm('Used for training: %d x %d x %d (total: %d)' % (
+                data.shape[0], data.shape[1], data.shape[2], data.size))
+
+            self.data.append(data)
+            self.data_sizes.append(data.size)
+
+        self.data_sizes = np.array(self.data_sizes)
+        self.data_sizes = self.data_sizes / np.sum(self.data_sizes)
+
+    def __getitem__(self, i):
+        pass
+
+    def __len__(self):
+        return self.len_epoch
+
+    def _select_orientation(self):
+        self.orientation = np.random.choice(self.orientations)
+
+    def _select_dataset(self):
+        if self.sampling_mode == 'uniform':
+            self.k = np.random.randint(0, len(self.data))
+        else:
+            self.k = np.random.choice(len(self.data), p=self.data_sizes)
+
+
+class UnlabeledMultiVolumeDataset(MultiVolumeDataset):
+    """
+    Dataset for multiple unlabeled volumes
+
+    :param data_path: list of paths to the datasets
+    :param input_shape: 3-tuple that specifies the input shape for sampling
+    :param optional split_orientation: orientation to train/test split the data along
+    :param optional split_location: location where the train/test data should be split
+    :param optional scaling: tuple used for rescaling the data, or None
+    :param optional len_epoch: number of iterations for one epoch
+    :param optional types: list of types of the volume files (tif2d, tif3d, tifseq, hdf5, png or pngseq)
+    :param optional in_channels: amount of subsequent slices to be sampled (only for 2D sampling)
+    :param optional sampling_mode: allow for uniform balance in sampling or not ("uniform" or "random")
+    :param optional orientations: list of orientations for sampling
+    :param optional batch_size: size of the sampling batch
+    :param optional dtype: type of the data (typically uint8)
+    :param optional norm_type: type of the normalization (unit, z or minmax)
+    :param optional train: train or test data
+    :param optional available: amount of available data and labels
+    """
+
+    def __init__(self, data_path, input_shape=None, split_orientation='z', split_location=0.50, scaling=None,
+                 len_epoch=1000, types='tif3d', sampling_mode='uniform', in_channels=1, orientations=(0,), batch_size=1,
+                 dtype='uint8', norm_type='unit', train=True, available=-1):
+        super().__init__(data_path, input_shape, scaling=scaling, split_orientation=split_orientation,
+                         split_location=split_location, len_epoch=len_epoch, types=types, sampling_mode=sampling_mode,
+                         in_channels=in_channels, orientations=orientations, batch_size=batch_size, dtype=dtype,
+                         norm_type=norm_type, train=train, available=available)
+
+    def __getitem__(self, i):
+
+        # reorient when we start a new batch
+        if i % self.batch_size == 0:
+            self._select_dataset()
+            self._select_orientation()
+
+        # get shape of sample
+        input_shape = _validate_shape(self.input_shape, self.data[self.k].shape, in_channels=self.in_channels,
+                                      orientation=self.orientation)
+
+        # get random sample
+        input = sample_unlabeled_input(self.data[self.k], input_shape)
+        input = normalize(input, type=self.norm_type)
+
+        # reorient sample
+        input = _orient(input, orientation=self.orientation)
+
+        if input.shape[0] > 1:
             # add channel axis if the data is 3D
             return input[np.newaxis, ...]
         else:
---
+++ train/train_autoencoder.py
@@ -0,0 +1,116 @@
+"""
+    This is a script that trains an autoencoder
+"""
+
+"""
+    Necessary libraries
+"""
+import argparse
+import datetime
+import os
+import json
+
+import torch.optim as optim
+from neuralnets.util.augmentation import *
+from neuralnets.util.tools import set_seed
+from neuralnets.util.losses import DiceLoss
+from neuralnets.networks.bvae import BVAE
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+
+from data.datasets import UnlabeledMultiVolumeDataset
+
+"""
+    Parse all the arguments
+"""
+print('[%s] Parsing arguments' % (datetime.datetime.now()))
+parser = argparse.ArgumentParser()
+
+# logging parameters
+parser.add_argument("--seed", help="Seed for randomization", type=int, default=0)
+parser.add_argument("--device", help="GPU device for computations", type=int, default=0)
+parser.add_argument("--log_dir", help="Logging directory", type=str, default="ae")
+parser.add_argument("--print_stats", help="Number of iterations between each time to log training losses",
+                    type=int, default=50)
+
+# network parameters
+parser.add_argument("--data_file", help="Path to the JSON data file", type=str, default="multi-volume.json")
+parser.add_argument("--input_size", help="Size of the blocks that propagate through the network",
+                    type=str, default="64,64")
+parser.add_argument("--fm", help="Number of initial feature maps in the segmentation U-Net", type=int, default=128)
+parser.add_argument("--levels", help="Number of levels in the segmentation U-Net (i.e. number of pooling stages)",
+                    type=int, default=4)
+parser.add_argument("--dropout", help="Dropout", type=float, default=0.25)
+parser.add_argument("--norm", help="Normalization in the network (batch or instance)", type=str, default="batch")
+parser.add_argument("--activation", help="Non-linear activations in the network", type=str, default="relu")
+parser.add_argument("--bottleneck", help="Feature dimensionality of the bottleneck layer", type=int, default=128)
+parser.add_argument("--beta", help="Beta parameter value for KL divergence", type=float, default=1e-4)
+
+# optimization parameters
+parser.add_argument("--lr", help="Learning rate of the optimization", type=float, default=1e-3)
+parser.add_argument("--step_size", help="Number of epochs after which the learning rate should decay",
+                    type=int, default=10)
+parser.add_argument("--gamma", help="Learning rate decay factor", type=float, default=0.9)
+parser.add_argument("--epochs", help="Total number of epochs to train", type=int, default=250)
+parser.add_argument("--len_epoch", help="Number of iteration in each epoch", type=int, default=100)
+parser.add_argument("--test_freq", help="Number of epochs between each test stage", type=int, default=1)
+parser.add_argument("--train_batch_size", help="Batch size in the training stage", type=int, default=4)
+parser.add_argument("--test_batch_size", help="Batch size in the testing stage", type=int, default=4)
+
+args = parser.parse_args()
+print('[%s] Arguments: ' % (datetime.datetime.now()))
+print('[%s] %s' % (datetime.datetime.now(), args))
+args.input_size = [int(item) for item in args.input_size.split(',')]
+
+"""
+Fix seed (for reproducibility)
+"""
+set_seed(args.seed)
+
+"""
+    Setup logging directory
+"""
+print('[%s] Setting up log directories' % (datetime.datetime.now()))
+if not os.path.exists(args.log_dir):
+    os.mkdir(args.log_dir)
+
+"""
+    Load the data
+"""
+df = json.load(open(args.data_file))
+input_shape = (1, args.input_size[0], args.input_size[1])
+print('[%s] Loading data' % (datetime.datetime.now()))
+augmenter = Compose(
+    [ToFloatTensor(device=args.device), Rotate90(), FlipX(prob=0.5), FlipY(prob=0.5), ContrastAdjust(adj=0.1),
+     RandomDeformation_2D(input_shape[1:], grid_size=(64, 64), sigma=0.01, device=args.device)])
+train = UnlabeledMultiVolumeDataset(df['raw'], split_orientation=df['split-orientation'],
+                                    split_location=df['split-location'], input_shape=input_shape,
+                                    len_epoch=args.len_epoch, types=df['types'], train=True)
+test = UnlabeledMultiVolumeDataset(df['raw'], split_orientation=df['split-orientation'],
+                                   split_location=df['split-location'], input_shape=input_shape,
+                                   len_epoch=args.len_epoch, types=df['types'], train=False)
+train_loader = DataLoader(train, batch_size=args.train_batch_size)
+test_loader = DataLoader(test, batch_size=args.test_batch_size)
+
+"""
+    Build the network
+"""
+print('[%s] Building the network' % (datetime.datetime.now()))
+net = BVAE(beta=args.beta, input_size=args.input_size, bottleneck_dim=args.bottleneck, feature_maps=args.fm,
+           levels=args.levels, dropout_enc=args.dropout, norm=args.norm, activation=args.activation)
+
+"""
+    Setup optimization for training
+"""
+print('[%s] Setting up optimization for training' % (datetime.datetime.now()))
+optimizer = optim.Adam(net.parameters(), lr=args.lr)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+
+"""
+    Train the network
+"""
+print('[%s] Starting training' % (datetime.datetime.now()))
+net.train_net(train_loader, test_loader, optimizer, epochs=args.epochs, scheduler=scheduler, augmenter=augmenter,
+              print_stats=args.print_stats, log_dir=args.log_dir, device=args.device)
+
+print('[%s] Finished!' % (datetime.datetime.now()))
